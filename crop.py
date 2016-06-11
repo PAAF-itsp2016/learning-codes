85df72ca405e4846a0d011ff6d68dfe19769f06e
abhay1997 <s.abhaymenon@gmail.com>
2016-06-11 18:24:54 +0530

    Update crop.py

--- crop.py
+++ crop.py
@@ -12,7 +12,7 @@ import cv2
 ap = argparse.ArgumentParser()
 ap.add_argument("-v", "--video",
 	help="path to the (optional) video file")
-ap.add_argument("-b", "--buffer", type=int, default=128,
+ap.add_argument("-b", "--buffer", type=int, default=32,
 	help="max buffer size")
 args = vars(ap.parse_args())
 
