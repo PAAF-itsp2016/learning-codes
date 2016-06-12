4e6a904e482b5852e1e5baac27fbc441b9ad084b
abhay1997 <s.abhaymenon@gmail.com>
2016-06-12 13:04:38 +0530

    Add files via upload

---
+++ abc444.py
@@ -0,0 +1,10 @@
+import cv2
+import Image
+import numpy as np
+
+img = cv2.imread('/home/sam/Desktop/itsp/pyfiles/index2.jpg')
+ball = img[235:280, 280:330]
+img[65:110, 170:220] = ball
+
+cv2.imshow('new', img)
+cv2.waitKey(0)
