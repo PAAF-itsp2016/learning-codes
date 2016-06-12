173e74d7a6e631a004d136f2c35fb37aacc8fa05
abhay1997 <s.abhaymenon@gmail.com>
2016-06-12 13:05:41 +0530

    Update and rename abc444.py to croptrial.py

--- croptrial.py
+++ croptrial.py
@@ -2,7 +2,7 @@ import cv2
 import Image
 import numpy as np
 
-img = cv2.imread('/home/sam/Desktop/itsp/pyfiles/index2.jpg')
+img = cv2.imread('index2.jpg')
 ball = img[235:280, 280:330]
 img[65:110, 170:220] = ball
 
